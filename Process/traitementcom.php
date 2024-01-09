313df3cfa12559003caa7562eb422a7640a6932e
Guerrouache <kamelguerrouachepro@gmail.com>
2024-01-09 16:52:25 +0100

    ???

--- Process/traitementcom.php
+++ Process/traitementcom.php
@@ -5,26 +5,29 @@ session_start();
 
 if (
     isset($_POST['pseudo']) && !empty($_POST['pseudo']) &&
-    isset($_POST['commentaire']) && !empty($_POST['commentaire'])) 
-    
+    isset($_POST['commentaire']) && !empty($_POST['commentaire'])&&
+    isset($_GET['id_musique']) && !empty($_GET['id_musique'])) 
     
     {
-        $idUser = $_SESSION['id_user'];
-        
-        $request = $database->query("INSERT INTO commentaire VALUES ('id_user, `Text`, 'id_musique') ");
+        $idUser = $_POST['pseudo'];
+        $musiqueID = $_GET['id_musique'];
+
+        $request = $database->prepare("INSERT INTO commentaire (com, id_user, id_musique) VALUES (':com', ':id_user', ':id_musique')  ");
         $idexist = $request->execute([
             'id_user' => $_SESSION['id_user'],
-            `Text` => $_POST['commentaire'],
-            'id_musique' => $_GET['id']
+            `com` => $_POST['commentaire'],
+            'id_musique' => $musiqueID
                 ]);    
 
         
 
 
 
-
     }
 
+var_dump($_GET['id_musique']);
+
+die();
 
 
 
--- partials/play.php
+++ partials/play.php
@@ -25,6 +25,11 @@ include_once "../partials/header-retour-acceuil.php";
   }
   ?>
 <div class="d-flex justify-content-center">
+<a href = 'http://localhost/SpotyHess/SpotyHess/partials/play.php?id=<?php echo intval($musicId)-1 ?>'id="retour" style="background: none; border: none;">
+<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
+</svg>
+ </a>
   <div class="" style="width: 25rem;">
     <img src="<?php echo $muzik['picture'] ?>" class="card-img-top" alt="...">
     <div class="card-body">
@@ -33,8 +38,67 @@ include_once "../partials/header-retour-acceuil.php";
       <audio controls src="<?php echo $muzik["audio"] ?>"></audio>
     </div>
   </div>
+<a href = 'http://localhost/SpotyHess/SpotyHess/partials/play.php?id=<?php echo intval($musicId)+1 ?>'id="suivant" style="background: none; border: none;">
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+    </svg>
+ </a>
+
 </div>
 
+
+
+</form>
+<h1>Laissez un commentaire</h1>
+  
+  <form action="../Process/traitementcom.php" method="POST">
+    <label for="pseudo">Pseudo :</label>
+    <input type="text" id="pseudo" name="pseudo" required><br><br>
+    
+    <label for="commentaire">Commentaire :</label><br>
+    <textarea id="commentaire" name="commentaire" rows="4" cols="50" required></textarea><br><br>
+    
+    <input type="submit" value="Envoyer">
+  </form>
+
+
+
+  <script> 
+ 
+let btnsuivant =  document.querySelector('#suivant');
+
+let currentCount = <?php $musicId ?>
+
+    btnsuivant.addEventListener('click', (suiv)=> {
+      $musicId ++;
+    btnsuivant.href = 'http://localhost/SpotyHess/SpotyHess/partials/play.php?id='. $musicId ;
+
+    if ($musicId = 16) {
+      suiv(); $musicId = 1 ;
+    }else {
+
+    }
+   
+
+});
+
+
+    btnsuivant.addEventListener('click', (suiv)=> {
+      $musicId --;
+    btnsuivant.href = 'http://localhost/SpotyHess/SpotyHess/partials/play.php?id='. $musicId ;
+   
+    if ($musicId = 1) {
+      suiv(); $musicId = 16 ;
+    }else {
+
+    }
+
+});
+
+
+
+</script>
+  
 </body>
 
 </html>
